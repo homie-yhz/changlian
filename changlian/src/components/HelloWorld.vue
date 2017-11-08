0b5a2fc7cb28eab823f9d811ba709436ba6b49b8
yindi <17744559519@163.com>
2017-11-09 00:10:14 +0800

    新增字段

--- changlian/src/components/HelloWorld.vue
+++ changlian/src/components/HelloWorld.vue
@@ -17,7 +17,7 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
-    <div>这里是改变</div>
+    <div>这里是改变再次改变</div>
   </div>
 </template>
 
