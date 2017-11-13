26c4297ba58a92cb6b020731fb020f4cd55a57f6
yindi <17744559519@163.com>
2017-11-13 21:02:35 +0800

    提IC卡相关页面

--- changlian/src/components/ICRecharge.vue
+++ changlian/src/components/ICRecharge.vue
@@ -1,11 +1,95 @@
 <template>
-  <div>IC卡充值页面</div>
+  <div style="background-color:#f7f7f7;height:100%;">
+    <header class="header v-fcm por">
+      <div class="v-fcm m-auto h-100" style="width:80%">IC卡绑定</div>
+      <div @click="back()" class="poa lt-0 v-fcm h-100" style="width:10%;">
+        <span class="arrow-back"></span>
+      </div>
+    </header>
+    <div class="bind-IC-box">
+      <div>
+        <div class="">
+          <div class="v-fm">
+            <span>卡号</span><span class="w-100 v-fm" v-text="ICCardInfo.num"></span>
+          </div>
+          <div class="v-fm">
+            <span>余额</span><span class="w-100 v-fm" v-text="(ICCardInfo.balance||'')+'元'"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-  
-}
-</script>
-<style>
+import Vue from "Vue";
+import axios from "axios";
 
+export default {
+  data() {
+    return {
+      ICCardInfo:{}
+    };
+  },
+  created() {
+    let ICCareInfoUro = "";
+    let _this = this;
+    axios
+      .get(ICCareInfoUro)
+      .then(function(data) {
+        let data1= {
+          "num":"321321321312321",
+          "balance":"200.00"
+        }
+        _this.ICCardInfo = data1;
+      })
+      .catch(function(data) {});
+  }
+};
+</script>
+<style lang="scss">
+.header {
+  height: 2rem;
+  border-bottom: 1px solid #d9d9d9;
+  background-color: #fff;
+}
+.arrow-back {
+  width: 0.4rem;
+  height: 0.4rem;
+  border-left: 2px solid #616161;
+  border-top: 2px solid #616161;
+  transform: rotate(-45deg);
+  margin-left: 0.5rem;
+}
+.bind-IC-box {
+  padding: 0.5rem 0.6rem 1rem;
+  & > div {
+    & > div {
+      border: 1px solid #e3e3e3;
+      border-radius: 2px;
+      margin-bottom: 1.5rem;
+      & > div {
+        background-color: #fff;
+        height: 2rem;
+        & > span {
+          width: 3rem;
+          text-align: center;
+        }
+        & input {
+          width: 100%;
+        }
+        &:last-child {
+          border-top: 1px solid #e3e3e3;
+        }
+      }
+    }
+  }
+}
+.btn-bind {
+  width: 100%;
+  height: 1.8rem;
+  background-color: #ffc700;
+  color: #fff;
+  border-radius: 5px;
+  font-size: 0.7rem;
+}
 </style>
--- changlian/src/components/usuallyStation.vue
+++ changlian/src/components/usuallyStation.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>usuallyStation</div>
+  <div>
+    <header class="header v-fcm por">
+      <div class="v-fcm m-auto h-100" style="width:80%">常用电站</div>
+      <div @click="back()" class="poa lt-0 v-fcm h-100" style="width:10%;">
+        <span class="arrow-back"></span>
+      </div>
+    </header>
+    <ul class="station-list">
+      <li v-for="stationInfo in stationList">
+        <router-link :to="{name:stationInfo}" class="v-fm" style="width:100%;">
+        <div class="ml-6 v-i1">
+          <!-- 地址 -->
+          <p class="fw-b">{{stationInfo.addr}}</p>
+          <!-- 允许支付方式 -->
+          <p class="icon-label-box">
+            <span>现金</span>
+            <span>APP支付+</span>
+          </p>
+          <!-- 充电口情况 -->
+          <p class="v-fm mt-2">
+            <span class="v-fm mr-6">
+              <span class="icon-total v-fcm">共</span><span style="width:1rem;">{{stationInfo.totalElec}}</span>
+            </span>
+            <span class="v-fm mr-6">
+              <span class="icon-idle v-fcm">闲</span><span style="width:1rem;">{{stationInfo.idleElec}}</span>
+            </span>
+          </p>
+        </div>
+        <!-- 快慢充 -->
+        <div v-if="stationInfo.chargeType==='fast'" class="v-fcm fast-charge">快充</div>
+        <div v-if="stationInfo.chargeType==='slow'" class="v-fcm slow-charge">慢充</div>
+        <div class="v-fcm" style="width:2.8rem;border-left:1px solid #e3e3e3;">
+          <div>
+            <i class="icon-star" style="margin-top:.2rem;"></i>
+            <!-- 据我多远 -->
+            <span>{{stationInfo.distanceToMe}}</span>
+          </div>
+        </div>
+        </router-link>
+      </li>
+      <li class="v-fcm">附近4公里范围内没有更多站点了</li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
@@ -10,6 +52,17 @@ export default {
   }
 }
 </script>
-<style>
-
+<style lang="scss">
+.header{
+  height: 2rem;
+  border-bottom:1px solid #d9d9d9;
+}
+.arrow-back{
+  width:.4rem;
+  height:.4rem;
+  border-left:2px solid #616161;
+  border-top:2px solid #616161;
+  transform: rotate(-45deg);
+  margin-left:.5rem;
+}
 </style>
