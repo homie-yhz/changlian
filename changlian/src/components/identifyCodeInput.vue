2050e3dbe0cc28a6d12eccd3853dc4880eb69227
yindi <17744559519@163.com>
2017-11-16 13:56:57 +0800

    增加验证码页面的倒计时功能

--- changlian/src/components/identifyCodeInput.vue
+++ changlian/src/components/identifyCodeInput.vue
@@ -10,39 +10,117 @@
       <!-- 输入验证码 -->
       <div class="identif-box" style="padding:0 1.6rem;">
         <div class="v-fb">
-          <span>验证码已发送</span><span style="color:#e65300">57s后重新发送</span>
+          <span>验证码已发送</span><span @click="getCode"  style="color:#e65300">{{getCodeBtn.text}}</span>
+          <!-- :class="{}" -->
         </div>
-        <div class="v-f identifycode-box">
-          <input type="tel" class="v-i1" maxlength="1">
-          <input type="tel" class="v-i1" maxlength="1">
-          <input type="tel" class="v-i1" maxlength="1">
-          <input type="tel" class="v-i1" maxlength="1">
+        <div class="v-f identifycode-box" refs="inputBox">
+          <input v-for="index in [0,1,2,3]" type="tel" class="v-i1" maxlength="1" v-model="inputs[index]">
         </div>
       </div>
       <!-- 下一步按钮 -->
-      <div class="btn btn-next">下一步</div>
+      <div class="btn btn-next" @click="nextStep" :class="{disable:nextStepBtn.state===0}">下一步</div>
     </div>
   </div>
 </template>
 <script>
 import $ from "jquery";
+import axios from 'axios';
+import GLOBAL from '../GLOBAL';
+//倒计时时间设定
+const leftTime = 60;
+
 export default {
   data() {
-    return {};
+    return {
+      getCodeBtn: {
+        state: 0,                    //0:不可以点击  1：允许点击  2：倒计时
+        leftTime: leftTime,
+        text: leftTime + "s后重新获取" //倒计时剩余时间,
+      },
+      nextStepBtn:{
+        state:0,                     //0:不可以点击 1：允许点击
+      },
+      inputs:['','','',''],
+    };
+  },
+  methods: {
+    //点击下一步
+    nextStep(){
+      let _this = this;
+      if(this.nextStepBtn.state === 1){
+        console.log('点击下一步');
+      }
+    },
+    //获取验证码
+    getCode() {
+      if(this.getCodeBtn.state === 1){
+        console.log('允许点击');
+        this.getCodeBtn.text = leftTime + "s后重新获取";
+        this.countDown();
+        this.getIndentifyCode_IF();
+      }
+    },
+    //倒计时
+    countDown() {
+      this.getCodeBtn.state = 2;
+      let _this = this;
+      setTimeout(() => {
+        _this.getCodeBtn.text = --_this.getCodeBtn.leftTime + "s后重新获取";
+        if (_this.getCodeBtn.leftTime > 0) {
+          _this.countDown();
+        } else {
+          // 重置数据
+          _this.getCodeBtn = {
+            state: 1,           //0:不可以点击  1：允许点击  2：倒计时
+            leftTime: leftTime,
+            text:"重新获取"      //倒计时剩余时间,
+          }
+        }
+      }, 1000);
+    },
+    //获取验证码接口
+    getIndentifyCode_IF(){
+        //params:phone
+        let getIndentifyCodeUrl = GLOBAL.interfacePath + '';
+        axios
+          .get(getIndentifyCodeUrl)
+          .then(function(data){
+            console.log('getIndentifyCodeUrl|返回数据|'+JSON.stringify(data.data));
+            if(!!data.data){
+              alert('验证码'+data.data);
+            }
+          })
+          .catch(function(err){
+            console.log({'url':getIndentifyCodeUrl,'err':JSON.stringify(err)});
+          });
+    }
   },
   created() {
+    this.countDown();
+    this.getIndentifyCode_IF();
+    // 监听验证码输入框
     this.$nextTick(function() {
-      $("input").eq(0).focus();
+      $("input")
+        .eq(0)
+        .focus();
       $("input").bind("input propertychange", function() {
-        //进行相关操作
-        console.log('3');
-        console.log($(this).index());
-        if(!!$(this).val()){
+        if (!!$(this).val()) {
           $(this).next().focus();
         }
-
       });
     });
+  },
+  watch:{
+    inputs:function(nv){
+      console.log(nv); 
+      for(var i = 0;i<nv.length;i++){
+        if(!nv[i]){
+          this.nextStepBtn.state = 0;
+          return false;
+        }
+      }
+      this.nextStepBtn.state = 1; 
+    }
   }
 };
 </script>
